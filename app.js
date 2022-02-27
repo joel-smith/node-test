90f966d12faa4d611af74c438227d20acc3b459f
Joel Smith <jsmith8547@conestogac.on.ca>
2022-02-27 14:04:45 -0500

    refactored port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.port ||3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome to Homepage");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(port, () => {
     console.log("Dis me");
 });
 
